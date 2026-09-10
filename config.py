2ea00e90db351b7d1ec7da15bdccabeb42398e73
syedafauzia71 <syedafauzia71@gmail.com>
2026-09-10 07:05:19 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
-DB_HOST = os.environ.get('KTX_DB_HOST', 'mysql-fornewworkpy-syedafauzia71-da87.a.aivencloud.com')
-DB_PORT = int(os.environ.get('KTX_DB_PORT', 11847))
-DB_USER = os.environ.get('KTX_DB_USER', 'avnadmin')
-DB_PASSWORD = os.environ.get('KTX_DB_PASSWORD', 'AVNS_KfUwf7Xx5RusmdWQvtM')
+DB_HOST = os.environ.get('KTX_DB_HOST', '127.0.0.1')
+DB_PORT = int(os.environ.get('KTX_DB_PORT', 3360))
+DB_USER = os.environ.get('KTX_DB_USER', 'root')
+DB_PASSWORD = os.environ.get('KTX_DB_PASSWORD', '')
 DB_NAME = os.environ.get('KTX_DB_NAME', 'toy_store')
 
 USE_DATABASE = os.environ.get('KTX_USE_DB', '1') not in ('0', 'false', 'False')
